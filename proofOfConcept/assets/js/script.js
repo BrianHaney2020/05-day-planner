0473be04819cffa23263e1f8a839db003ea17976
Brian Haney <brian.haney@kinsaleins.com>
2021-03-24 15:22:47 -0400

    working version. Fully functional; format needs improvement.

---
+++ proofOfConcept/assets/js/script.js
@@ -0,0 +1,37 @@
+localStorage.setItem("Hour9", "sample text");
+localStorage.setItem("Hour11", "stuff for 11");
+
+myObject = localStorage.getItem.Hour04;
+
+console.log(myObject);
+
+var test = "Hour09";
+/*
+var poc = localStorage.getItem(test);
+console.log(test+" is equal to "+poc);
+console.log("truthiness="+(Boolean(poc)));
+
+test = "Hour08";
+var poc = localStorage.getItem(test);
+console.log(test+" is equal to "+poc);
+console.log("truthiness="+(Boolean(poc)));
+
+for (var i = 1; i<10; i++) {
+    test = "Hour0"+i;
+    var poc = localStorage.getItem(test);
+console.log(test+" is equal to "+poc);
+console.log(test+" is equal to "+poc);
+console.log("truthiness="+(Boolean(poc)));
+} 
+*/
+
+/*
+note to self--I'm trying to see if you can preset the text that goes
+in a form 
+
+var formObject = document.querySelector("#Hour09");
+//formObject.defaultValue("Blak");
+console.log(formObject);*/
+
+
+
